f42f970b07af2571f7ff2451e33141ac053fa01e
koen.metsu <koen.metsu@gmail.com>
2015-05-29 23:11:32 +0200

    Update assembly version

--- src/Muffin.Pictures.Archiver/AssemblyInfo.fs
+++ src/Muffin.Pictures.Archiver/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Muffin.Pictures.Archiver")>]
 [<assembly: AssemblyProductAttribute("Muffin.Pictures.Archiver")>]
 [<assembly: AssemblyDescriptionAttribute("Archiver for Muffin's pictures")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.1.0"
