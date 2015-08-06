2dd8ee417b5a64b51e186738f7f15b44746f46ff
koen.metsu <koen.metsu@gmail.com>
2015-08-06 08:15:14 +0200

    Rename createDirectory to better convey purpose

--- src/Muffin.Pictures.Archiver/FileMover.fs
+++ src/Muffin.Pictures.Archiver/FileMover.fs
@@ -7,14 +7,14 @@ open Moves
 
 module FileMover =
 
-    let private createDirectory destination =
+    let private ensureDirectoryExists destination =
         let fileInfo = FileInfo destination
         if not (fileInfo.Directory.Exists) then
             Directory.CreateDirectory(fileInfo.Directory.FullName) |> ignore
 
     let private moveFile {Source=source; Destination=destination} =
-        createDirectory destination
-        File.Move(source, destination)
+        ensureDirectoryExists destination
+        File.Copy(source, destination)
 
     let move targetPath pictures =
         pictures
