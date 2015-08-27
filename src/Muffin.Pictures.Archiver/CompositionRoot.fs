b5215b0b2594887cf085ea81599a626aa7b534dc
koen.metsu <koen.metsu@gmail.com>
2015-08-27 07:26:21 +0200

    Move composition to CompositionRoot module

---
+++ src/Muffin.Pictures.Archiver/CompositionRoot.fs
@@ -0,0 +1,43 @@
+﻿namespace Muffin.Pictures.Archiver
+
+open System
+
+open Muffin.Pictures.Archiver.Pictures
+open Muffin.Pictures.Archiver.FileMover
+open Muffin.Pictures.Archiver.Moves
+open Muffin.Pictures.Archiver.Files
+open Muffin.Pictures.Archiver.TimeTakenRetriever
+open Muffin.Pictures.Archiver.Domain
+open Muffin.Pictures.Archiver.FileSystem
+
+open Muffin.Pictures.Archiver.Runner
+
+module CompositionRoot =
+
+    let fsOperations = FileSystemOperations
+
+    let composeCleanUp =
+        let deleteSource moveRequest = fsOperations.Delete moveRequest.Source
+        cleanUp deleteSource
+
+    let composeMoveWithFs =
+        let ensureDirectoryExists = ensureDirectoryExists fsOperations.DirectoryExists fsOperations.CreateDirectory
+        let copy source destination overwrite = fsOperations.Copy(source, destination, overwrite)
+        let copyToDestination moveRequest = copyToDestination ensureDirectoryExists copy moveRequest
+
+        moveFile copyToDestination
+
+    let composeGetPictures arguments =
+        let timeProvider () = DateTimeOffset.UtcNow
+        let timeTakenProvider = timeTaken arguments.Mode
+        getOldPictures timeTakenProvider timeProvider allFilesInPath
+
+    let composeMove =
+        let moveWithFs = composeMoveWithFs
+        let cleanUp = composeCleanUp
+        let composeCompareFiles = compareFiles fsOperations.ReadAllBytes
+        move moveWithFs composeCompareFiles cleanUp
+
+    let composeGetMoveRequests arguments =
+        let getPictures = composeGetPictures arguments
+        getMoveRequests getPictures
--- src/Muffin.Pictures.Archiver/Program.fs
+++ src/Muffin.Pictures.Archiver/Program.fs
@@ -1,46 +1,15 @@
 ﻿open System
 
-open Muffin.Pictures.Archiver.Pictures
-open Muffin.Pictures.Archiver.FileMover
-open Muffin.Pictures.Archiver.Moves
-open Muffin.Pictures.Archiver.Files
-open Muffin.Pictures.Archiver.TimeTakenRetriever
-open Muffin.Pictures.Archiver.Domain
-open Muffin.Pictures.Archiver.FileSystem
-open Muffin.Pictures.Archiver.Runner
 open Muffin.Pictures.Archiver.Arguments
-
-let fsOperations = FileSystemOperations
-
-let composeCleanUp =
-    let deleteSource moveRequest = fsOperations.Delete moveRequest.Source
-    cleanUp deleteSource
-
-let composeMoveWithFs =
-    let ensureDirectoryExists = ensureDirectoryExists fsOperations.DirectoryExists fsOperations.CreateDirectory
-    let copy source destination overwrite = fsOperations.Copy(source, destination, overwrite)
-    let copyToDestination moveRequest = copyToDestination ensureDirectoryExists copy moveRequest
-
-    moveFile copyToDestination
-
-let composeGetPictures arguments =
-    let timeProvider () = DateTimeOffset.UtcNow
-    let timeTakenProvider = timeTaken arguments.Mode
-    getOldPictures timeTakenProvider timeProvider allFilesInPath
+open Muffin.Pictures.Archiver.CompositionRoot
+open Muffin.Pictures.Archiver.Runner
 
 [<EntryPoint>]
 let main argv =
     let arguments = parseArguments argv
 
-    // compose move
-    let moveWithFs = composeMoveWithFs
-    let cleanUp = composeCleanUp
-    let composeCompareFiles = compareFiles fsOperations.ReadAllBytes
-    let move = move moveWithFs composeCompareFiles cleanUp
-
-    // compose moveRequests
-    let getPictures = composeGetPictures arguments
-    let getMoveRequests = getMoveRequests getPictures
+    let move = composeMove
+    let getMoveRequests = composeGetMoveRequests arguments
 
     runner move getMoveRequests arguments
 
