d19055b31ddd64ab0ed2a5044e4a6099151d8c77
koen.metsu <koen.metsu@gmail.com>
2015-08-28 08:25:57 +0200

    Add some documentation

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -1,5 +1,5 @@
 (*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use 
+// This block of code is omitted in the generated HTML documentation. Use
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
 
@@ -7,57 +7,61 @@
 Muffin.Pictures.Archiver
 ======================
 
-Documentation
+Muffin - *the name of our NAS (wife got to choose the name).* <br />
+Pictures - *what the archiver can work with*.<br />
+Archiver - *what this application does.*
 
-<div class="row">
-  <div class="span1"></div>
-  <div class="span6">
-    <div class="well well-small" id="nuget">
-      The Muffin.Pictures.Archiver library can be <a href="https://nuget.org/packages/Muffin.Pictures.Archiver">installed from NuGet</a>:
-      <pre>PM> Install-Package Muffin.Pictures.Archiver</pre>
-    </div>
-  </div>
-  <div class="span1"></div>
-</div>
+Archiver tries to find out when a pictures were taken based on the EXIF tag present in jpgs, and moves them to folders based on the year-month.
+If archiver cannot find the time taken through EXIF tags, it can optionally fall back on the modified date of the file.
 
 Example
 -------
 
-This example demonstrates using a function defined in this sample library.
-
+Given a number of files
 *)
-#r "Muffin.Pictures.Archiver.dll"
-open Muffin.Pictures.Archiver
 
-printfn "hello = %i" <| Library.hello 0
+| File  | DateTaken        | Date Modified    |
+| ----- | ---------------- | ---------------- |
+| File1 | 2015-02-03 17:23 | 2014-02-03 17:23 |
+| File2 |                  | 2014-07-03 17:23 |
+| File3 | 2014-07-14 08:25 | 2015-01-20 22:59 |
 
 (**
-Some more info
+the following output will be created by the Archiver (with fallback mode enabled):
 
-Samples & documentation
+*)
+
+.
+|-- 2015-02
+|   |--- File1
+|-- 2014-07
+|   |--- File2
+|   |--- File3
+
+(**
+
+Running the application
 -----------------------
 
-The library comes with comprehensible documentation. 
-It can include tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
+Build the application with ``build.cmd`` or ``build.sh``.
 
- * [Tutorial](tutorial.html) contains a further explanation of this sample library.
+Run the Archiver eg in the command line:
 
- * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
-   and functions in the library. This includes additional brief samples on using most of the
-   functions.
- 
+```
+Muffin.Pictures.Archiver.exe --sourcedir c:\temp --destinationdir c:\temp\destination --fallback --mailto abc@def.com
+```
+
 Contributing and copyright
 --------------------------
 
-The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
-the project and submit pull requests. If you're adding a new public API, please also 
+The project is hosted on [GitHub][gh] where you can [report issues][issues], fork
+the project and submit pull requests. If you're adding a new public API, please also
 consider adding [samples][content] that can be turned into a documentation. You might
 also want to read the [library design notes][readme] to understand how it works.
 
-The library is available under Public Domain license, which allows modification and 
-redistribution for both commercial and non-commercial purposes. For more information see the 
-[License file][license] in the GitHub repository. 
+The library is available under Public Domain license, which allows modification and
+redistribution for both commercial and non-commercial purposes. For more information see the
+[License file][license] in the GitHub repository.
 
   [content]: https://github.com/fsprojects/Muffin.Pictures.Archiver/tree/master/docs/content
   [gh]: https://github.com/fsprojects/Muffin.Pictures.Archiver
--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -4,16 +4,20 @@
 #I "../../bin"
 
 (**
-Introducing your project
-========================
+Configuration
+-------------
 
-Say more
+The Archiver can be configured with the following settings, either through command line parameters or through the ``Muffin.Pictures.Archiver.config``. 
+However, command line parameters have precedence over XML configuration.
 
-*)
-#r "Muffin.Pictures.Archiver.dll"
-open Muffin.Pictures.Archiver
-
-Library.hello 0
-(**
-Some more info
+- SourceDir
+  - (**Mandatory**) The directory from where to move the files.
+- DestinationDir
+  - (**Mandatory**) The directory where the files will be archived into folders by date.
+- Fallback
+  - Set this flag if you want the Archiver to look at the date modified if it can't find the correct EXIF tag.
+  - This will also consequently move any other file type.
+- MailTo
+  - Set this if you want to send an email report of the archival process.
+  - This will require you to configure smtp in the ``smtp.config`` file. 
 *)
