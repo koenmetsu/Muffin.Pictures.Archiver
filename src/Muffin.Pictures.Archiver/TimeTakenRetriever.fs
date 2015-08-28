27976da4910c9989ffdabf5538f212cd213bca68
koenmetsu <koen.metsu@infrabel.be>
2015-08-28 13:32:28 +0200

    Close ExifToolLib after use

--- src/Muffin.Pictures.Archiver/TimeTakenRetriever.fs
+++ src/Muffin.Pictures.Archiver/TimeTakenRetriever.fs
@@ -21,21 +21,24 @@ module TimeTakenRetriever =
                DateTimeOffset.Parse(dateTaken)
 
     let private timeTakenFromPath timeTakenMode path =
-        ExifToolLib.ExifToolIO.Initiailize() |> ignore // todo;
-        let fileTagValues = ExifToolVBNetDemo.FileTagValues(path, [|"XMP-xmp:CreateDate";"ExifIFD:CreateDate"|])
+        try
+            ExifToolLib.ExifToolIO.Initiailize() |> ignore // todo;
+            let fileTagValues = ExifToolVBNetDemo.FileTagValues(path, [|"XMP-xmp:CreateDate";"ExifIFD:CreateDate"|])
 
-        fileTagValues.TagValueList
-        |> Seq.cast
-        |> Seq.tryPick tryGetPrintValue
-        |> function
-            | Some x ->
-                Some <| parseDate x
-            | None ->
-                match timeTakenMode with
-                | Strict ->
-                    None
-                | Fallback ->
-                    Some <| DateTimeOffset(System.IO.File.GetLastWriteTimeUtc(path))
+            fileTagValues.TagValueList
+            |> Seq.cast
+            |> Seq.tryPick tryGetPrintValue
+            |> function
+                | Some x ->
+                    Some <| parseDate x
+                | None ->
+                    match timeTakenMode with
+                    | Strict ->
+                        None
+                    | Fallback ->
+                        Some <| DateTimeOffset(System.IO.File.GetLastWriteTimeUtc(path))
+        finally
+            ExifToolLib.ExifToolIO.Close() |> ignore
 
     let timeTaken timeTakenMode file =
         timeTakenFromPath timeTakenMode file.FullPath
