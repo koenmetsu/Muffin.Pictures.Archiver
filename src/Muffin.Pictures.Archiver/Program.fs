c79973fa5ecefa18cbf94bc53d4baa9b74e40fa5
koen.metsu <koen.metsu@gmail.com>
2015-09-04 12:21:14 +0200

    Call exiftool with processInfo

--- src/Muffin.Pictures.Archiver/Program.fs
+++ src/Muffin.Pictures.Archiver/Program.fs
@@ -4,13 +4,39 @@ open Muffin.Pictures.Archiver.Arguments
 open Muffin.Pictures.Archiver.CompositionRoot
 open Muffin.Pictures.Archiver.Runner
 
+open System.Diagnostics
+
 [<EntryPoint>]
 let main argv =
-    let arguments = parseArguments argv
 
-    let move = composeMove
-    let getMoveRequests = composeGetMoveRequests arguments
+    let callExifTool folder =
+        let processStartInfo = new ProcessStartInfo()
+        processStartInfo.FileName <- "D:\Projects\Muffin.Pictures.Archiver\lib\exiftool.exe"
+        processStartInfo.Arguments <- sprintf "-fast22 -m -q -j -d \"%%Y.%%m.%%d %%H:%%M:%%S\" %s" folder
+        processStartInfo.CreateNoWindow <- true
+        processStartInfo.UseShellExecute <- false
+        processStartInfo.RedirectStandardOutput <- true
+        processStartInfo.RedirectStandardError <- true
+        processStartInfo.RedirectStandardInput <- true
+        let builder = new System.Text.StringBuilder()
+        let exifProcess = Process.Start(processStartInfo)
+        exifProcess.OutputDataReceived.Add(fun e ->
+            builder.Append(e.Data) |> ignore)
+        exifProcess.BeginOutputReadLine()
+        exifProcess.WaitForExit() //(5000) |> ignore
+        let json = builder.ToString()//exifProcess.StandardOutput.ReadToEnd()
+        Console.WriteLine(json)
 
-    runner move getMoveRequests arguments
-
+    let json = callExifTool @"D:\Projects\Muffin.Pictures.Archiver\tests\Muffin.Pictures.Archiver.Tests\testdata\"
+    Console.ReadLine() |> ignore
     0
+
+//
+//    let arguments = parseArguments argv
+//
+//    let move = composeMove
+//    let getMoveRequests = composeGetMoveRequests arguments
+//
+//    runner move getMoveRequests arguments
+//
+//    0
---
+++ tests/Muffin.Pictures.Archiver.Tests/TagTests.fs
@@ -0,0 +1,50 @@
+﻿namespace Muffin.Pictures.Archiver.Tests
+
+open Swensen.Unquote
+open NUnit.Framework
+open FSharp.Data
+
+open Muffin.Pictures.Archiver.Tests.TestHelpers
+open Muffin.Pictures.Archiver.Domain
+open Muffin.Pictures.Archiver.Rop
+
+module TagRetriever =
+    open System.Diagnostics
+
+    type Tags = JsonProvider<"test.json">
+
+    let callExifTool folder =
+        let processStartInfo = new ProcessStartInfo()
+        processStartInfo.FileName <- "exiftool.exe"
+        processStartInfo.Arguments <- sprintf "-fast22 -m -q -j -d \"%%Y.%%m.%%d %%H:%%M:%%S\" %s" folder
+        processStartInfo.CreateNoWindow <- true
+        processStartInfo.UseShellExecute <- false
+        processStartInfo.RedirectStandardOutput <- true
+        processStartInfo.RedirectStandardError <- true
+        processStartInfo.RedirectStandardInput <- true
+        let exifProcess = Process.Start(processStartInfo)
+        let builder = new System.Text.StringBuilder()
+        exifProcess.OutputDataReceived.Add(fun e ->
+            builder.AppendLine(e.Data) |> ignore)
+        exifProcess.BeginOutputReadLine()
+        exifProcess.WaitForExit()
+        let json = builder.ToString()
+        json
+
+module TagTests =
+    open TagRetriever
+
+    [<Test>]
+    let ``Call exifTool and try to parse results`` () =
+        let json = callExifTool @"testdata"
+
+        let tags = Tags.Parse(json)
+
+        test <@ Seq.length tags = 3 @>
+
+    [<Test>]
+    let ``Try parsing test`` () =
+        let tags = Tags.Load("test.json")
+
+        test <@ Seq.length tags = 5 @>
+
