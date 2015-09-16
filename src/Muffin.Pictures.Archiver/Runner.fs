01812731e426de8461769402c14aba39e052757f
koenmetsu <koen.metsu@infrabel.be>
2015-09-16 12:10:49 +0200

    Use tee to add commands to pipeline

--- src/Muffin.Pictures.Archiver/Runner.fs
+++ src/Muffin.Pictures.Archiver/Runner.fs
@@ -4,30 +4,35 @@ open Muffin.Pictures.Archiver.Domain
 open Muffin.Pictures.Archiver.ConsoleReporter
 open Muffin.Pictures.Archiver.Report
 open Muffin.Pictures.Archiver.MailReporter
+open Muffin.Pictures.Archiver.Rop
 
 module Runner =
 
     let runner move getMoveRequests arguments =
+        let runInParallel =
+            let asyncMove request = async { return move request }
+
+            List.map asyncMove
+            >> Async.Parallel
+            >> Async.RunSynchronously
+            >> List.ofArray
+
+        let reportToMailIfNecessary report =
+            if arguments.MailTo.IsSome then
+                reportToMail report arguments.MailTo.Value
+
+        let watch = System.Diagnostics.Stopwatch()
+
         let moveRequests =
             getMoveRequests arguments.SourceDir arguments.DestinationDir
-        let watch = System.Diagnostics.Stopwatch()
+
         watch.Start()
-        let asyncMove request = async { return move request }
-        let successfulMoveRequests = moveRequests |> List.choose isSuccess
+        moveRequests
+            |> List.choose isSuccess
+            |> runInParallel
+            |> tee (fun _ -> watch.Stop())
+            |> createReport moveRequests
+            |> tee reportToConsole
+            |> reportToMailIfNecessary
 
-        let moveResults =
-            successfulMoveRequests
-            |> List.map asyncMove
-            |> Async.Parallel
-            |> Async.RunSynchronously
-            |> List.ofArray
-
-        watch.Stop()
-
-        let report = createReport moveRequests moveResults
-
-        reportToConsole report
         System.Console.WriteLine watch.ElapsedMilliseconds
-        if arguments.MailTo.IsSome then
-            reportToMail report arguments.MailTo.Value
-
