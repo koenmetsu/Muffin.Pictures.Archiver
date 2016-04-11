95e7beeba81474688c3a0ac8f1f19da3b861c771
Koen Metsu <koen.metsu@gmail.com>
2016-04-11 16:25:35 +0200

    Build exiftool in tmp folder instead of lib

--- install-exif.sh
+++ install-exif.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-pushd lib
+mkdir tmp
+pushd tmp
 wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.02.tar.gz
 gzip -dc Image-ExifTool-10.02.tar.gz | tar -xf -
 pushd Image-ExifTool-10.02
 perl Makefile.PL
 make test
-make install
+make PREFIX=. install
 cp exiftool ../exiftool
 cp lib ../lib -r
 popd
+rm tmp -rf
 rm Image-ExifTool-10.02* -rf
