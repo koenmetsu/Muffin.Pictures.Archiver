6d828076b4ecce4d1500cd35f1c7df1ba0937026
Koen Metsu <koen.metsu@gmail.com>
2016-04-11 19:26:41 +0200

    Stop using make

--- install-exif.sh
+++ install-exif.sh
@@ -3,12 +3,14 @@ mkdir tmp
 pushd tmp
 wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.02.tar.gz
 gzip -dc Image-ExifTool-10.02.tar.gz | tar -xf -
+mv Image-ExifTool-10.02/exiftool ../lib/exiftool
+mv Image-ExifTool-10.02/lib ../lib/
 pushd Image-ExifTool-10.02
-perl Makefile.PL
-make test
-make PREFIX=. install
-cp exiftool ../lib/exiftool
+# perl Makefile.PL PREFIX=~/usr/bin LIB=~/usr/lib/exiftool/
+# make test
+# make install
+popd
+#cp exiftool ../lib/exiftool -r
 # cp lib ../lib -r
 popd
-rm tmp -rf
-rm Image-ExifTool-10.02* -rf
+# rm tmp -rf
