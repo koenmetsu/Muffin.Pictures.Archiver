8ba92247547db3dc45c127a59d472b4943a9d16b
koen.metsu <koen.metsu@gmail.com>
2016-05-05 20:57:49 +0200

    Fix test

--- tests/Muffin.Pictures.Archiver.Tests/MovesTests.fs
+++ tests/Muffin.Pictures.Archiver.Tests/MovesTests.fs
@@ -25,8 +25,8 @@ module MovesTests =
         let expected : Result<MoveRequest,Failure> list =
             [
                     Success { Source = @"/path/to/originDir/pic.jpg";
-                              Destination = System.String.Format(@"/path/to/destinationDir{0}2014-01{0}pic.jpg", System.IO.Path.DirectorySeparatorChar); TimeTaken = System.DateTimeOffset.Now }
+                              Destination = System.String.Format(@"/path/to/destinationDir{0}2014-01{0}pic.jpg", System.IO.Path.DirectorySeparatorChar); TimeTaken = dateTimeOffset 2014 01 01 }
                     Success { Source = @"/path/to/originDir/pic2.jpg";
-                              Destination = System.String.Format(@"/path/to/destinationDir{0}2014-12{0}pic2.jpg", System.IO.Path.DirectorySeparatorChar); TimeTaken = System.DateTimeOffset.Now }
+                              Destination = System.String.Format(@"/path/to/destinationDir{0}2014-12{0}pic2.jpg", System.IO.Path.DirectorySeparatorChar); TimeTaken = dateTimeOffset 2014 12 31 }
             ]
         test <@ expected = actual @>
