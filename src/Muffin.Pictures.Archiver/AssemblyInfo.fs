50fd78beebfa3af8ad0e7fb354f71fe463e414a4
koen.metsu <koen.metsu@gmail.com>
2016-05-17 22:29:54 +0200

    Add reference to System.Runtime in all projects

--- src/Muffin.Pictures.Archiver/AssemblyInfo.fs
+++ src/Muffin.Pictures.Archiver/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Muffin.Pictures.Archiver")>]
 [<assembly: AssemblyProductAttribute("Muffin.Pictures.Archiver")>]
 [<assembly: AssemblyDescriptionAttribute("Archiver for Muffin's pictures")>]
-[<assembly: AssemblyVersionAttribute("1.3.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.3.1")>]
+[<assembly: AssemblyVersionAttribute("1.3.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.3.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.3.1"
-    let [<Literal>] InformationalVersion = "1.3.1"
+    let [<Literal>] Version = "1.3.2"
+    let [<Literal>] InformationalVersion = "1.3.2"
